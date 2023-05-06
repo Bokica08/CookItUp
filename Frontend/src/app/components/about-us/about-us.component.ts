6c15884f24d56e8d1f8207b6a364d7f5120cea39
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-05-06 16:09:42 +0200

    Small design fixes

--- Frontend/src/app/components/about-us/about-us.component.ts
+++ Frontend/src/app/components/about-us/about-us.component.ts
@@ -11,22 +11,14 @@ import { RecipeService } from 'src/app/services/recipe.service';
 })
 export class AboutUsComponent {
   images = [
-    { src: 'https://img.freepik.com/free-vector/male-chef-mixing-flour-eggs-dough-with-whisk-hands-happy-man-apron-preparing-homemade-sweet-dessert-table-home-kitchen-flat-vector-illustration-pastry-cooking-recipe-concept_74855-21965.jpg?w=1060&t=st=1682299686~exp=1682300286~hmac=cf7ece8ccb58e55284c985529e8fb1ab76998cc9b9bf0d20d44437925d3f7f2f', alt: 'Customers', text: 'Satisfied customers', count:0 },
-    { src: 'https://img.freepik.com/free-photo/soup-veggies-arrangement-flat-lay_23-2148452857.jpg?w=996&t=st=1682299735~exp=1682300335~hmac=505cd761a84a9c1b51023e455049c254cc40947544f750c3a510b5241f8ec6f1', alt: 'Recipes', text: 'Recipes to browse', count:0 },
-    { src: 'https://img.freepik.com/free-vector/hands-choosing-products-category-while-doing-online-shopping-man-using-shop-application-smartphone-flat-vector-illustration-customer-making-electronic-payment-ecommerce-technology-concept_74855-24197.jpg?w=740&t=st=1682299840~exp=1682300440~hmac=55b47535c4c26c896aff9fd92f64ec160edd1b587e7038d1f475e3a24da64e74', alt: 'Categories', text: 'Categories to choose from', count:0 },
-    { src: 'https://img.freepik.com/free-vector/gradient-delivery-concept-with-phone_23-2149164058.jpg?w=740&t=st=1682299870~exp=1682300470~hmac=a63fa1a02b1ebc477b5e22e77e801b068c9196c8630509b525f88410c7c6c20a', alt: 'Orders', text: 'Processed orders', count:0 }
+    { src: 'https://scontent.fskp4-1.fna.fbcdn.net/v/t1.15752-9/345077306_1282397092405509_3226986097444120482_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=yyP0t6QKc90AX8BOpcH&_nc_ht=scontent.fskp4-1.fna&oh=03_AdS4CS3Gh73MW8Iuq6iZ8NF5kvxUK8eFYMFIArwxIWFP2A&oe=647D6ADE', alt: 'Recipes', text: 'Diverse recipes' },
+    { src: 'https://scontent.fskp4-2.fna.fbcdn.net/v/t1.15752-9/344289060_562142506060476_1627302007777585682_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=VQtGkxXn_xAAX-1Zmqs&_nc_ht=scontent.fskp4-2.fna&oh=03_AdTlJFNZS7FdLcirwYu4g5qOFSeKKUAUgzTfb9fWHeJnvg&oe=647DD966', alt: 'Categories', text: 'Various categories' },
+    { src: 'https://scontent.fskp4-1.fna.fbcdn.net/v/t1.15752-9/343857919_1201893140471363_5815012129178626399_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=GYxJMULVN0sAX-mjkIe&_nc_ht=scontent.fskp4-1.fna&oh=03_AdTtUaaFAhDmOfdrm9Abrs0jLy9j4tpAndwU20z5UhVvNQ&oe=647D73C2', alt: 'Orders', text: 'Quick orders' }
   ];
   constructor(private route: ActivatedRoute,
     private recipeService: RecipeService,
     private customerService: CustomerService,
     private orderService: OrderService){}
   ngOnInit(): void {
-    this.getAllCounts() 
-  }
-  getAllCounts(){
-    this.recipeService.getCategoriesCount().subscribe((n)=>this.images[2].count=n)
-    this.recipeService.getRecipesCount().subscribe((n)=>this.images[1].count=n)
-    this.orderService.getNumberOfOrders().subscribe((n)=>this.images[3].count=n)
-    this.customerService.getNumberOfCustomers().subscribe((n)=>this.images[0].count=n)
   }
 }
